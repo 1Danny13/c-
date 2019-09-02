fab09987b767be465eac188e188faa0134ff4797
1Danny13 <38400809+1Danny13@users.noreply.github.com>
2019-09-02 10:42:06 +0800

    Add files via upload

---
+++ ConsoleApplication5/pch.cpp
@@ -0,0 +1,170 @@
+﻿// pch.cpp: 与预编译标头对应的源文件；编译成功所必需的
+
+#include "pch.h"
+#include<iostream>
+
+//插入num数组的前len个元素
+Tree::Tree(int num[], int len)
+{
+	root = new Node(num[0]);
+	//把数组中的其他数组插入到二叉排序树中
+	for (int i = 1; i < len; i++)
+		insertNode1(num[i]);
+	//insertNode(num[i]);
+}
+void Tree::insertNode(int data)
+{
+	Node *p, *par;
+	Node* newNode = new Node(data);
+
+	p = par = root;
+	while (p != NULL)
+	{
+		par = p;   //保存节点
+		if (data > p->data)
+			p = p->right;
+		else if (data < p->data)
+			p = p->left;
+		else if (data == p->data)
+		{
+			delete newNode;
+			return;
+		}
+	}
+	newNode->parent = par;
+	if (par->data > newNode->data)
+		par->left = newNode;
+	else
+		par->right = newNode;
+}
+
+//递归实现插入
+void Tree::insertNode(int data)
+{
+	if (root != NULL)
+		insertNode(root, data);
+}
+void Tree::insertNode(Node* current, int data)
+{
+	if (data < current->data) {
+		if (current->left == NULL)
+		{
+			current->left = new Node(data);
+			current->left->parent = current;
+		}
+		else
+			insertNode(current->left, data);
+	}
+	else if(data > current->data){
+		if (current->right == NULL)
+		{
+			current->right = new Node(data);
+			current->right->parent = current;
+		}
+		else
+			insertNode(current->right, data);
+	}
+	return;
+}
+
+//递归查找
+Node* Tree::searchNode(Node* current, int data) {
+	if (data < current->data)
+	{
+		if (current->left == NULL)
+			return NULL;
+		return searchNode(current->left, data);
+	}
+	else if(data > current->data){
+		if (current->right == NULL)
+			return NULL;
+		return searchNode(current->right, data);
+	}
+	return current;
+}
+
+//删除数据为data的节点及子树
+
+void Tree::deleteNode(int data)
+{
+	Node* current = NULL;
+
+	current = searchNode(data);
+	if (current != NULL)
+		deleteNode(current);
+}
+void Tree::deleteNode(Node* current)
+{
+	if (current->left != NULL)
+		deleteNode(current->left);
+	if (current->right != NULL)
+		deleteNode(current->right);
+	if (current->parent == NULL)
+	{
+		delete current;
+		root = NULL;
+		return;
+	}
+	//将current父亲节点的相应指针置空
+	if (current->parent->data > current->data)  //current为父节点
+		current->parent->left = NULL;
+	else
+		current->parent->right = NULL;
+
+	delete current;
+}
+//中序遍历递归
+void Tree::InOrderTree()
+{
+	if (root == NULL)
+		return;
+	InOrderTree(root);
+}
+void Tree::InOrderTree(Node* current)
+{
+	if (current != NULL)
+	{
+		InOrderTree(current->left);
+		std::cout << current->data << std::endl;
+		InOrderTree(current->right);
+	}
+}
+//先序遍历递归
+void Tree::InOrderTree()
+{
+	if (root == NULL)
+		return;
+	InOrderTree(root);
+}
+void Tree::InOrderTree(Node* current)
+{
+	if (current != NULL)
+	{
+		std::cout << current->data << std::endl;
+		InOrderTree(current->left);
+		InOrderTree(current->right);
+	}
+}
+//后序遍历递归
+void Tree::InOrderTree()
+{
+	if (root == NULL)
+		return;
+	InOrderTree(root);
+}
+void Tree::InOrderTree(Node* current)
+{
+	if (current != NULL)
+	{
+		InOrderTree(current->left);
+		InOrderTree(current->right);
+		std::cout << current->data << std::endl;
+	}
+}
+
+//先序遍历非递归
+
+//中序遍历非递归
+
+//后序遍历非递归
+// 一般情况下，忽略此文件，但如果你使用的是预编译标头，请保留它。
---
+++ ConsoleApplication5/pch.h
@@ -0,0 +1,54 @@
+﻿// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+#ifndef PCH_H
+#define PCH_H
+#include <iostream>
+class Node
+{
+public:int data;
+	   Node *parent;
+	   Node *left;
+	   Node *right;
+public:
+	Node() :data(-1), parent(NULL), left(NULL), right(NULL) {};
+	Node(int num) :data(num), parent(NULL), left(NULL), right(NULL) {};
+	//~Node() {};
+};
+//二叉树类定义
+class Tree
+{
+private:  //递归方法
+	void insertNode(Node* current, int data);
+	Node* searchNode(Node* current, int data);
+	void deleteNode(Node* current);
+private:
+	Node* root;
+public:
+	Tree(int num[], int len);
+	void insertNode(int data);   //非递归
+	void insertNode1(int data);
+	Node *searchNode(int data);
+	void deleteNode(int data);
+
+	//遍历算法
+	void InOrderTree();
+	void InOrderTree(Node* current);
+	void InOrderTreeUnrecursion();
+
+	void PreOrderTree();
+	void PreOrderTree(Node* current);
+	void PreOrderTreeUnrecursion();
+
+	void PostOrderTree();
+	void PostOrderTree(Node* current);
+	void PostOrderTreeUnrecursion();
+};
+// TODO: 添加要在此处预编译的标头
+
+#endif //PCH_H
